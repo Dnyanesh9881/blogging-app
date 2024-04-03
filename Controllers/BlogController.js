20ee59df76ce9bc05a72d987ea9c792e1d1fb71c
Dnyaneshwar Suryawanshi <suryawanshidnyaneshwar59@gmail.com>
2024-04-04 00:58:02 +0530

    blog created and fetched

--- Controllers/BlogController.js
+++ Controllers/BlogController.js
@@ -1,48 +1,95 @@
-const express=require("express");
+const express = require("express");
 const BlogDataValidation = require("../Utils/BlogUtil");
 const User = require("../Models/UserModel");
-const createBLog = require("../Models/BlogModel");
+const {createBLog, getAllBlogs, getMyBlogs} = require("../Models/BlogModel");
 
-const BlogRouter=express.Router();
+const BlogRouter = express.Router();
 
-BlogRouter.post("/create-blog", async(req, res)=>{
-     const{title, textBody}=req.body;
-     const createDateTime= Date.now();
-     const userId=req.session.user.userId;
-     try {
-        await BlogDataValidation({title, textBody});
-        const userDb=await User.findUserWithkey({key:userId});
-    
-     } catch (error) {
+BlogRouter.post("/create-blog", async (req, res) => {
+  const { title, textBody } = req.body;
+  const createDateTime = Date.now();
+  const userId = req.session.user.userId;
+  try {
+    await BlogDataValidation({ title, textBody });
+    const userDb = await User.findUserWithkey({ key: userId });
+  } catch (error) {
+    return res.send({
+      status: 400,
+      message: error,
+    });
+  }
+  try {
+    const blogDb = await createBLog({
+      title,
+      textBody,
+      createDateTime,
+      userId,
+    });
+    return res.send({
+      status: 201,
+      message: "Blog created successfully",
+      data: blogDb,
+    });
+  } catch (error) {
+    return res.send({
+      status: 400,
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
+});
+
+BlogRouter.get("/get-blog", async (req, res) => {
+    const SKIP=Number(req.query.skip)|| 0;
+    console.log("in get-blog");
+  try {
+    const blogDb = await getAllBlogs({SKIP});
+    console.log("in get-blog");
+    if(!blogDb){
         return res.send({
-            status:400,
-            message:error
-        })
-     }
-     try {
-        const blogDb=await createBLog({
-            title,
-            textBody,
-            createDateTime,
-            userId
-        });
-       return res.send({
-         status:201,
-         message:"Blog created successfully",
-         data:blogDb
-        })
-     } catch (error) {
+            status: 202,
+            message: "No more todos",
+          });
+    }
+    return res.send({
+      status: 200,
+      message: "Read Successfull",
+      data: blogDb,
+    });
+  } catch (error) {
+    return res.send({
+      status: 500,
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
+});
+
+BlogRouter.get("/my-blog", async(req, res)=>{
+    let userId=req.session.user.userId;
+    let SKIP=parseInt(req.query.skip)||0;
+    try {
+        const blogDb=await getMyBlogs({userId, SKIP});
+        if (blogDb.length === 0) {
+            return res.send({
+              status: 202,
+              message: "No more todos",
+            });
+          }
+        console.log()
         return res.send({
-            status :400,
+            status:200,
+            message:"blogs fetched successfully",
+            data:blogDb
+        })
+    } catch (error) {
+        return res.send({
+            status:500,
             message:"Internal Server Error",
             error:error
         })
-     }
+    }
 
 })
 
-BlogRouter.get("/get-blog", (req, res)=>{
-
-})
-
-module.exports=BlogRouter;
+module.exports = BlogRouter;
--- Models/BlogModel.js
+++ Models/BlogModel.js
@@ -1,20 +1,59 @@
 const BlogSchema = require("../Schemas/BlogSchema");
+const { LIMIT } = require("../constants");
 
-const createBLog=({title, textBody, createDateTime, userId})=>{
-    return new Promise (async(resolve, reject)=>{
-        const blogObj=new BlogSchema({
-            title,
-            textBody,
-            createDateTime,
-            userId
-        });
-        try {
-            const blogDb= await blogObj.save();
-            resolve(blogDb);
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
+const createBLog = ({ title, textBody, createDateTime, userId }) => {
+  return new Promise(async (resolve, reject) => {
+    const blogObj = new BlogSchema({
+      title,
+      textBody,
+      createDateTime,
+      userId,
+    });
+    try {
+      const blogDb = await blogObj.save();
+      resolve(blogDb);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+const getAllBlogs = ({ SKIP }) => {
+  return new Promise(async (resolve, reject) => {
+    console.log(SKIP);
+    try {
+      const blogDb = await BlogSchema.aggregate([
+        { $sort: { createDateTime: -1 } },
+        {
+          $facet: {
+            data: [{ $skip: SKIP }, { $limit: LIMIT }],
+          },
+        },
+      ]);
+      console.log(blogDb);
+      resolve(blogDb);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+const getMyBlogs = ({ userId, SKIP }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const blogDb = await BlogSchema.aggregate([
+        { $match: { userId: userId } },
+        { $sort: { createDateTime: -1 } },
+        {
+          $facet: {
+            data: [{ $skip: SKIP }, { $limit: LIMIT }],
+          },
+        },
+      ]);
+      console.log(blogDb);
+      resolve(blogDb);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
-module.exports=createBLog;
+module.exports = { createBLog, getAllBlogs, getMyBlogs };
